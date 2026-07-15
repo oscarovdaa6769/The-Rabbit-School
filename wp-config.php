83d794a27491e027e0180d95880de15de6e5983c
Neath <neath@gmail.com>
2026-07-15 15:39:24 +0700

    yes

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'rabbit_school' );
+define( 'DB_NAME', 'rabbit_db' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
